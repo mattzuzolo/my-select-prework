b9ffb9608f0c9d707101b408111a4d891dbc68ac
Matt Zuzolo <mattzuzolo@outlook.com>
2018-04-26 18:37:20 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -8,7 +8,7 @@ def my_select(collection)
         #need to check for true value THEN add it to the array
         if yield(collection[i])
             new_collection << yield(collection[i])
-     
+        end
         i += 1
      end
      
