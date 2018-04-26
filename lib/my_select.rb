03a674753f0a69b1631d97bde3bdcabc920f1357
Matt Zuzolo <mattzuzolo@outlook.com>
2018-04-26 18:39:00 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,17 @@
 def my_select(collection)
- # your code here!
+ 
+     i = 0
+    new_collection = []
+     
+     while collection.length > i
+     
+        #check if yield returns True or False — if true, shovel at given index
+        if yield(collection[i])
+            new_collection << (collection[i])
+        end
+        i += 1
+     end
+     
+     new_collection
+ 
 end
