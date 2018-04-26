c776cc3a203d81f0e3e4358b9e74eb238d954fae
Matt Zuzolo <mattzuzolo@outlook.com>
2018-04-26 18:35:41 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,15 @@
 def my_select(collection)
- # your code here!
+ 
+     i = 0
+    new_collection = []
+     
+     while collection.length > i
+        if yield(collection[i])
+        new_collection << yield(collection[i])
+     
+        i += 1
+     end
+     
+     new_collection
+ 
 end
